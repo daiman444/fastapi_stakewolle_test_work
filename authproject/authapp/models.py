4824a06ab7b56c431400058be84223f7d5aa5f84
ddd <daiman4444@gmail.com>
2024-02-08 22:26:43 +0600

    (!) - mogrations tested!!!!

--- authproject/authapp/models.py
+++ authproject/authapp/models.py
@@ -5,6 +5,8 @@ from django.db import models
 
 class User(models.Model):
     user_name = models.CharField(max_length=16)
+    email = models.EmailField(default='None')
+    token = models.IntegerField(default=0)
     
     def __str__(self) -> str:
         return self.user_name
