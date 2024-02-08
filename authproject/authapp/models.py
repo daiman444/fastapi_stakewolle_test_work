aec421e06ffab4517b29e2b39fc1ccfd6f137c9b
ddd <daiman4444@gmail.com>
2024-02-08 22:18:26 +0600

    tests for first migrations

--- authproject/authapp/models.py
+++ authproject/authapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    user_name = models.CharField(max_length=16)
+    
+    def __str__(self) -> str:
+        return self.user_name
+    
--- authproject/config/config.py
+++ authproject/config/config.py
@@ -13,11 +13,16 @@ if not os.environ.get("ENV_LOADED"):
 
 class Config(BaseSettings):
     
+    # General settings
+    TIME_ZONE: str
+    
+    # PostgreSQL
     PSQL_ENGINE: str
     PSQL_NAME: str
     PSQL_USER: str
     PSQL_PASSWORD: str
     PSQL_HOST: str
     PSQL_PORT: str
+    
     
 config = Config()
