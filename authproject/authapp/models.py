f801883755561286fb3cc25ab5078a2f3d51ccf3
ddd <daiman4444@gmail.com>
2024-02-08 22:42:32 +0600

    added model for token

--- authproject/authapp/models.py
+++ authproject/authapp/models.py
@@ -1,13 +1,24 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
 
 class User(models.Model):
     user_name = models.CharField(max_length=16)
-    email = models.EmailField(default='None')
-    token = models.IntegerField(default=0)
+    email = models.EmailField(unique=True, default='None')
+    date_registration = models.DateTimeField('date of registration', default=timezone.now)
+    
     
     def __str__(self) -> str:
         return self.user_name
+    
+    
+class Token(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.IntegerField(unique=True, default=0)
+    date_creation = models.DateTimeField('date of creation', default=timezone.now)
+    
+    def __str__(self) -> str:
+        return self.owner
     
