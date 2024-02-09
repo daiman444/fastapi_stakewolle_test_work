8b31e8dd808263f60c84be8b50ea3682eb23d65b
ddd <daiman4444@gmail.com>
2024-02-09 13:25:05 +0600

    tests for models in shell

--- authproject/authapp/models.py
+++ authproject/authapp/models.py
@@ -5,20 +5,43 @@ from django.utils import timezone
 
 
 class User(models.Model):
-    user_name = models.CharField(max_length=16)
-    email = models.EmailField(unique=True, default='None')
-    date_registration = models.DateTimeField('date of registration', default=timezone.now)
-    
-    
-    def __str__(self) -> str:
-        return self.user_name
+    user_name = models.CharField(
+        max_length=16
+    )
+    email = models.EmailField(
+        verbose_name="Input yor e-mail",
+        unique=True,
+        default='xxx@yyy.zzz'
+    )
+    date_registration = models.DateTimeField(
+        'date of registration', 
+        default=timezone.now
+    )
     
     
 class Token(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    token = models.IntegerField(unique=True, default=0)
-    date_creation = models.DateTimeField('date of creation', default=timezone.now)
-    
-    def __str__(self) -> str:
-        return self.owner
+    referrer = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE, 
+        related_name="tokens",
+        default=None,
+    )
+    referee = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name="referee_user",
+        null=True, default=None,
+        blank=True,
+    )
+    token = models.IntegerField(
+        unique=True, 
+        default=0,
+    )
+    token_status = models.BooleanField(
+        default=True,
+    )
+    date_creation = models.DateTimeField(
+        'date of creation', 
+        default=timezone.now,
+    )
     
