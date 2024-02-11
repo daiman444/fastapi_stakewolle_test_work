727a98726eb0dfaebbcf521b9a2f1465ce78adfd
ddd <daiman4444@gmail.com>
2024-02-11 22:12:36 +0600

    token app

--- authproject/authproject/settings.py
+++ authproject/authproject/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authapp.apps.AuthappConfig',
+    'authtoeknapp.apps.AuthtokenAppConfig',
 ]
 
 MIDDLEWARE = [
