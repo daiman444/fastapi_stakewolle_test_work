e112e1836ed2b5a97fa35c7faee0e93db304715a
ddd <daiman4444@gmail.com>
2024-02-11 22:18:19 +0600

    added token app

--- authproject/authproject/settings.py
+++ authproject/authproject/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'authapp.apps.AuthappConfig',
-    'authtoeknapp.apps.AuthtokenAppConfig',
+    'authtoken_app.apps.AuthtokenAppConfig',
 ]
 
 MIDDLEWARE = [
--- authproject/authproject/urls.py
+++ authproject/authproject/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("authapp.urls")),    
+    path('', include("authtokenapp.urls")),
+    path('login/', include("authapp.urls")),
 ]
