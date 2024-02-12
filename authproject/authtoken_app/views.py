a612f1fd3a432ffc0674e29cffabdf793c8edad9
ddd <daiman4444@gmail.com>
2024-02-12 11:44:11 +0600

    rebuild app

--- authproject/authtoken_app/views.py
+++ authproject/authtoken_app/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+def show_token(request: HttpRequest):
+    page_title = "Token"
+    return render(
+        request=request,
+        template_name="index.html",
+        context={
+            "page_title": page_title,
+        }
+    )
