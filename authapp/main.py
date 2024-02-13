44de82fa40189171c3a7fd904647b70ca9db78a3
ddd <daiman4444@gmail.com>
2024-02-13 22:58:45 +0600

    fastapi is a greate framework

--- authapp/main.py
+++ authapp/main.py
@@ -1,10 +1,10 @@
 from datetime import datetime
+from typing import Annotated
 
-from fastapi import FastAPI, Response, Request, Cookie, HTTPException
+from fastapi import FastAPI, Response, Request, Header
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
-from api.schemas.settings import settings
 from api.schemas.models import User, Feedback
 
 
@@ -12,51 +12,9 @@ app = FastAPI()
 template = Jinja2Templates(directory="public/templates")
 
 
-user_data = User(
-    id=2,
-    name="Vasyan",
-    password=123,
-    age=20
-)
-
-
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     # return {"message": "Hello World"}
     return template.TemplateResponse("index.html", {"request": request})
 
-
-@app.get("/login")
-async def login(response: Response, usermane: str, userpassword: int):
-    if usermane == user_data.name:
-        if userpassword == user_data.password:
-            now = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
-            response.set_cookie(key="set_time", value=now)
-            return {"message": "coockies are set"}
-        else:
-            return {"message": "invelid passowrd"}
-    else:
-        return {"message": "user is not found"}
-        
-
-
-@app.get("/user")
-async def user(set_time = Cookie(default=None)):
-    if set_time is None:
-        HTTPException(status_code=401, detail="you needed to signin or signup")
-    else:
-        user_info = {
-            "user id": user_data.id,
-            "user name": user_data.name,
-            "user age": user_data.age,
-        }
-        return {
-            "user info": user_info,
-            "set_time": set_time,
-        }
-    
-    
-@app.post("/signout", status_code=204)
-async def logout(response: Response):
-    response.delete_cookie("set_time")
-    return {"message": "you have left the session"}
+# TODO from here https://stepik.org/lesson/1044674/step/1?unit=1053248
