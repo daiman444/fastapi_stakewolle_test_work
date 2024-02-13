5fa08af6af56b126449f0b3ddf18197525840abd
ddd <daiman4444@gmail.com>
2024-02-13 17:07:01 +0600

    upd app structure

--- authapp/api/schemas/settings.py
+++ authapp/api/schemas/settings.py
@@ -4,12 +4,14 @@ from typing import Any, Dict, List, Optional
 from pydantic_settings import BaseSettings
 
 
-if os.path.isfile('.env'):
-    from dotenv import load_dotenv
-    load_dotenv()
-    print('Loading environment variables from .env')
-else:
-    print("No .env file found. Using system environment variables.")
+if not os.environ.get("ENV_LOADED"):
+    if os.path.isfile('.env'):
+        from dotenv import load_dotenv
+        load_dotenv()
+        os.environ["ENV_LOADED"] = "True"
+        print('Loading environment variables from .env')
+    else:
+        print("No .env file found. Using system environment variables.")
 
 
 class Settings(BaseSettings):
