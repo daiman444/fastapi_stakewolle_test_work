6a2ef99ecadf695ae09b63ac53351c0723c9f023
ddd <daiman4444@gmail.com>
2024-02-13 17:26:35 +0600

    upd

--- authapp/main.py
+++ authapp/main.py
@@ -1,12 +1,14 @@
 import os
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from api.schemas.settings import settings
 
 
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="public/templates"))
 
 
 @app.get(f"/{settings.APP_PATH}")
