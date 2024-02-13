e4b0742c24eaaf364d8e11bd478aced3cd519eb7
ddd <daiman4444@gmail.com>
2024-02-13 21:20:47 +0600

    added routes andmodels

---
+++ authapp/api/schemas/models.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int
+    name: str
+    age: int
+    
+    
+class Feedback(BaseModel):
+    name: str
+    message: str
--- authapp/main.py
+++ authapp/main.py
@@ -1,25 +1,41 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Cookie
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from api.schemas.settings import settings
-
+from api.schemas.models import User, Feedback
 
 
 app = FastAPI()
-#app.mount("/static", StaticFiles(directory="public/templates", html=True))
 template = Jinja2Templates(directory="public/templates")
 
+feedback_list = []
 
-@app.get(f"/{settings.APP_PATH}", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
     # return {"message": "Hello World"}
     return template.TemplateResponse("index.html", {"request": request})
 
 
-@app.get(f"/{settings.APP_PATH}/calculate/",)
+@app.get("/calculate",)
 async def root(request: Request, num1: int, num2: int ):
     # return {"message": "Hello World"}
     result = num1 + num2
     return {"result": result}
+
+
+@app.post("/user",)
+async def root(user: User):
+    user_data = user.model_dump()
+    if user.age > 18:
+        user_data["is_adult"] = True
+    else:
+        user_data["is_adult"] = False    
+    return {"user_data": user_data}
+
+
+@app.post("/feedback",)
+async def root(feedback: Feedback):
+    feedback_list.append(feedback)
+    print(feedback_list)
+    return {"message": "Feedback received"}
