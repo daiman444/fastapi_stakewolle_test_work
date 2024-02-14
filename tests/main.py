78bf4bdceaffbbe86fa5ee16b628f4097ca136e3
ddd <daiman4444@gmail.com>
2024-02-14 15:47:24 +0600

    upd

--- tests/main.py
+++ tests/main.py
@@ -1,19 +1,4 @@
 import pytest
 from asynctest import CoroutineMock, MagicMock
-
-from authapp.db_repo import UserRepo
-from authapp.db.db_session import a_session
-from authapp.db.redis import redis_app
-
-
-@pytest.mark.asyncio
-async def test_add_user():
-    user_data = MagicMock()
-    session = CoroutineMock()
-    redis_app = MagicMock()
-    
-    user_repo = UserRepo()
-    
-    user_repo.
     
     
