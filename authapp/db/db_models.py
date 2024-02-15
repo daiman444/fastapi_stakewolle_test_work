62e6308cfd649bcd69701cce3b9c695f29fddf63
ddd <daiman4444@gmail.com>
2024-02-15 22:26:12 +0600

    models aka declarative style

--- authapp/db/db_models.py
+++ authapp/db/db_models.py
@@ -1,12 +1,16 @@
+from typing import List
 from datetime import datetime, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from sqlalchemy import (
-    Column, Integer, String, 
-    ForeignKey, DateTime, Boolean,
-    Text
-)
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import DeclarativeBase, relationship
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.sql.sqltypes import (
+    String, Integer, Text,
+    DateTime, Boolean
+)
+
+from api.schemas.settings import settings
 
 
 class Base(DeclarativeBase):
@@ -16,23 +20,22 @@ class Base(DeclarativeBase):
 class Users(Base):
     __tablename__ = "users"
     
-    id = Column(Integer, 
-                primary_key=True, 
-                unique=True, 
-                index=True, 
-                autoincrement=True)
-    login = Column(String(32),
-                   nullable=False,
-                   unique=True)
-    email = Column(String(32),
-                   nullable=False,
-                   unique=True)
-    pass_hash = Column(Text,
-                       nullable=False,
-                       unique=True)
-    tokens = relationship("Token", 
-                          back_populates="owner",)
-    
+    id: Mapped[int] = mapped_column(primary_key=True,
+                                    type_=Integer,
+                                    unique=True,
+                                    index=True,
+                                    autoincrement=True)
+    login: Mapped[str] = mapped_column(type_=String(32),
+                                       nullable=False,
+                                       unique=True)
+    email: Mapped[str] = mapped_column(type_=String(32),
+                                       nullable=False,
+                                       unique=True)
+    pass_hash: Mapped[str] = mapped_column(type_=Text,
+                                           nullable=False,
+                                           unique=True,)
+    tokens: Mapped[List["Token"]] = relationship(back_populates="owner")
+
     def set_pass_hash(self, password: str):
         self.pass_hash = generate_password_hash(password=password)
         
@@ -43,27 +46,29 @@ class Users(Base):
 class Token(Base):
     __tablename__ = "token"
     
-    id = Column(Integer,
-                primary_key=True,
-                unique=True,
-                index=True,
-                autoincrement=True)
-    token = Column(String(32),
-                   unique=True,
-                   nullable=False)
-    created = Column(DateTime, 
-                     default=datetime.utcnow)
-    live_time = Column(Integer,
-                       default=timedelta(seconds=900) )
-    activated = Column(Boolean, 
-                       unique=False,
-                       default=False)
-    owner_id = Column(Integer, 
-                      ForeignKey("users.id",),
-                      nullable=False)
-    owner = relationship("Users",
-                         back_populates="tokens")
-    
+    id: Mapped[int] = mapped_column(type_=Integer,
+                                    primary_key=True,
+                                    unique=True,
+                                    index=True,
+                                    autoincrement=True)
+    token: Mapped[str] = mapped_column(type_=Text,
+                                       unique=True,
+                                       nullable=False)
+    created: Mapped[datetime.utcnow] = mapped_column(type_=DateTime,
+                                                     default=datetime.utcnow)
+    live_time: Mapped[int] = mapped_column(type_=Integer,
+                                           default=timedelta(
+                                               seconds=settings.TOKEN_LIFE_TIME
+                                               )
+                                           )
+    activated: Mapped[bool] = mapped_column(type_=Boolean,
+                                            unique=False,
+                                            default=False)
+    owner_id: Mapped[int] = mapped_column(ForeignKey("users.id"),
+                                          type_=Integer,
+                                          nullable=False)    
+    owner: Mapped["Users"] = relationship(back_populates="tokens")
+
     def token_is_alive(self):
         time_delda: timedelta = datetime.utcnow() - self.created
         return self.live_time > time_delda.total_seconds()
